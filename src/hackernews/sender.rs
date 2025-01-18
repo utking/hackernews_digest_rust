547da587e15f09490e9fd6b2ebc478724c5b647c
Gennady Utkin <utkingg@gmail.com>
2025-01-18 14:05:41 -0600

    Add support for sending to Telegram

--- src/hackernews/sender.rs
+++ src/hackernews/sender.rs
@@ -1,26 +1,52 @@
-use crate::{DigestItem, SmtpConfig};
+use crate::{DigestItem, SmtpConfig, TelegramConfig};
 use lettre::message::{MultiPart, SinglePart};
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{SmtpTransport, Transport};
+use teloxide::utils::markdown;
 
-pub struct Sender {
+pub trait DigestSender {
+    async fn send_digest(
+        &self,
+        digest: &Vec<DigestItem>,
+        send_to: &str,
+        subject: &str,
+    ) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+pub struct SmtpSender {
     config: SmtpConfig,
 }
 
-impl Sender {
+pub struct TelegramSender {
+    config: TelegramConfig,
+}
+
+impl SmtpSender {
     pub fn new(config: &SmtpConfig) -> Self {
         Self {
             config: config.clone(),
         }
     }
+}
 
-    pub async fn send_email(
+impl TelegramSender {
+    #[must_use]
+    pub fn new(config: &TelegramConfig) -> Self {
+        Self {
+            config: config.clone(),
+        }
+    }
+}
+
+impl DigestSender for SmtpSender {
+    async fn send_digest(
         &self,
+        digest: &Vec<DigestItem>,
         send_to: &str,
         subject: &str,
-        text_body: &str,
-        html_body: &str,
     ) -> Result<(), Box<dyn std::error::Error>> {
+        let text_body = digest_to_text(digest);
+        let html_body = digest_to_html(digest);
         let email = lettre::Message::builder()
             .from(self.config.from.parse()?)
             .to(send_to.parse()?)
@@ -45,6 +71,32 @@ impl Sender {
             Ok(_) => return Ok(()),
             Err(e) => eprintln!("Could not send email: {:?}", e),
         }
+
+        Ok(())
+    }
+}
+
+pub struct DummySender {}
+
+impl DigestSender for DummySender {
+    async fn send_digest(
+        &self,
+        digest: &Vec<DigestItem>,
+        _send_to: &str,
+        _subject: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let mut body = String::from("Hi!\n\n");
+
+        for item in digest {
+            body.push_str(&format!(
+                "* {title} - {url}\n",
+                url = item.news_url,
+                title = item.news_title
+            ));
+        }
+        body.push_str(format!("\nGenerated: {}", formatted_now()).as_str());
+
+        println!("{}", body);
 
         Ok(())
     }
@@ -86,4 +138,40 @@ pub fn digest_to_text(digest: &Vec<DigestItem>) -> String {
 
 fn formatted_now() -> String {
     chrono::Local::now().to_rfc2822()
+}
+
+impl DigestSender for TelegramSender {
+    async fn send_digest(
+        &self,
+        digest: &Vec<DigestItem>,
+        send_to: &str,
+        _subject: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        use teloxide::prelude::*;
+
+        let bot = Bot::new(&self.config.token);
+
+        for item in digest {
+            let body = format!(
+                "*[{}]({})*",
+                markdown::escape(&item.news_title),
+                markdown::escape_link_url(&item.news_url),
+            );
+
+            match bot
+                .send_message(send_to.to_string(), body.as_str())
+                .parse_mode(teloxide::types::ParseMode::MarkdownV2)
+                .send()
+                .await
+            {
+                Ok(_) => {}
+                Err(e) => {
+                    eprintln!("Could not send message: {:?}", e);
+                    return Err(Box::new(e));
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
