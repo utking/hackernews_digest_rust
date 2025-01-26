9160d7e7e3d7457d4a2ed0b544fc6d2fba1cb0fe
Gennady Utkin <utkingg@gmail.com>
2025-01-26 13:27:23 -0600

    Analyse news IDs on per-source basis only to prevent conflicts

--- src/common/repository.rs
+++ src/common/repository.rs
@@ -72,11 +72,16 @@ pub fn vacuum(
 }
 
 /// Get IDs of the news items whose IDs are not in the database yet
-pub fn get_ids_to_pull(prefetched_ids: Vec<i32>, conn: &mut AnyConnection) -> Vec<i32> {
-    use crate::schemas::prelude::rss_items::dsl::{id, rss_items};
+pub fn get_ids_to_pull(
+    news_source: &str,
+    prefetched_ids: Vec<i32>,
+    conn: &mut AnyConnection,
+) -> Vec<i32> {
+    use crate::schemas::prelude::rss_items::dsl::{id, rss_items, source};
 
     let existing_ids: Vec<i32> = rss_items
         .select(id)
+        .filter(source.eq(news_source))
         .filter(id.eq_any(&prefetched_ids))
         .load::<i32>(conn)
         .expect("Error loading IDs");
